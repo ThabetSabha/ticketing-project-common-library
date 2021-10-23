bc52c5c8d71278c7cb354c674c99c30c1a6a206f
ThabetSabha <thabetsabha98@gmail.com>
2021-10-24 01:14:08 +0300

    improving error logging, allowing a custom reason for not found Error

--- src/errors/NotFoundError.ts
+++ src/errors/NotFoundError.ts
@@ -3,7 +3,7 @@ import { CustomError } from "./CustomError";
 export class NotFoundError extends CustomError {
   statusCode = 404;
   public reason;
-  constructor(customReason: string) {
+  constructor(customReason?: string) {
     super("route not found");
     this.reason = customReason || "route not found";
   }
